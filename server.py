b57783e69c2f78d6e07ff46c21bfdf18dfb2f946
S-mishina <183244r4@stu.teikyo-u.ac.jp>
2020-11-18 15:35:36 +0900

    v4

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template #追加
 import pymysql #追加
+import time
+import datetime
 
 app = Flask(__name__)
 
@@ -15,22 +17,40 @@ def hello():
             charset='utf8',
             cursorclass=pymysql.cursors.DictCursor,
         )
-
+    day = datetime.date.today()
+    today1= day_after_tomorrow = datetime.timedelta(days=1)
+    day1=day-today1
+    print(str(day1))
     cur = db.cursor()
     cur1 = db.cursor()
+    cur2 = db.cursor()
+    cur3 = db.cursor()
     sql = "SELECT * FROM `mask`"
+    sql1 = "SELECT * FROM `mask` ORDER BY day desc LIMIT 0, 5 ;"
+    sql2 = "SELECT * FROM `mask` WHERE `day` = '"+str(day)+"'"+ ";"
+    sql3 = "SELECT * FROM `mask` WHERE `day` = '"+str(day1)+"'"+ ";"
     cur.execute(sql)
+    cur1.execute(sql1)
+    cur2.execute(sql2)
+    cur3.execute(sql3)
     members = cur.fetchall()
-    sql1 = "SELECT * FROM `mask` WHERE 1 ORDER BY day LIMIT 0, 1"
-    cur.execute(sql1)
-    members1 = cur.fetchall()
+    members1 = cur1.fetchall()
+    members2 = cur2.fetchall()
+    members3 = cur3.fetchall()
+    print("1")
+    print(members)
+    print("2")
     print(members1)
-
-
+    print("3")
+    print(members2)
+    print("4")
+    print(members3)
     cur.close()
+    cur1.close()
+    cur2.close()
+    cur3.close()
     db.close()
 
-    return render_template('hello.html', title='flask test', members=members) #変更
+    return render_template('hello.html', title='flask test', members=members, members1=members1, members2=members2, members3=members3) #変更
 
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run(debug=True)
