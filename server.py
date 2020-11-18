a6454d9889736d75449801cfdbb45ea07c24cfb5
S-mishina <183244r4@stu.teikyo-u.ac.jp>
2020-11-18 15:59:22 +0900

    v6

--- server.py
+++ server.py
@@ -25,18 +25,26 @@ def hello():
     cur1 = db.cursor()
     cur2 = db.cursor()
     cur3 = db.cursor()
+    cur4 = db.cursor()
+    cur5 = db.cursor()
     sql = "SELECT * FROM `mask`"
     sql1 = "SELECT * FROM `mask` ORDER BY day desc LIMIT 0, 5 ;"
     sql2 = "SELECT * FROM `mask` WHERE `day` = '"+str(day)+"'"+ ";"
     sql3 = "SELECT * FROM `mask` WHERE `day` = '"+str(day1)+"'"+ ";"
+    sql4= "SELECT COUNT(test) FROM mask WHERE test ='ok' and day ='"+str(day)+"'"
+    sql5= "SELECT COUNT(test) FROM mask WHERE test ='ng' and day ='"+str(day)+"'"
     cur.execute(sql)
     cur1.execute(sql1)
     cur2.execute(sql2)
     cur3.execute(sql3)
+    cur4.execute(sql4)
+    cur5.execute(sql5)
     members = cur.fetchall()
     members1 = cur1.fetchall()
     members2 = cur2.fetchall()
     members3 = cur3.fetchall()
+    members4 = cur4.fetchall()
+    members5 = cur5.fetchall()
     print("1")
     print(members)
     print("2")
@@ -45,12 +53,18 @@ def hello():
     print(members2)
     print("4")
     print(members3)
+    print("5")
+    print(members4)
+    print("6")
+    print(members5)
     cur.close()
     cur1.close()
     cur2.close()
     cur3.close()
+    cur4.close()
+    cur4.close()
     db.close()
 
-    return render_template('hello.html', title='flask test', members=members, members1=members1, members2=members2, members3=members3) #変更
+    return render_template('hello.html', title='flask test', members=members, members1=members1, members2=members2, members3=members3,members4=members4,members5=members5) #変更
 
 app.run(debug=True)
